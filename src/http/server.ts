9537a59de0f55143c497a5206fde2c107ca4b2a7
Nicolò Bartelucci <nicobar97@gmail.com>
2023-07-15 11:29:02 +0200

    test

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,7 +4,7 @@ import appFactory from './app';
 export default async (configuration: Configuration) => {
   try {
     const appInstance = await appFactory(configuration);
-    await appInstance.listen({ port: configuration.server.port ?? 3001, host: '0.0.0.0' });
+    await appInstance.listen({ port: configuration.server.port ?? 80, host: '0.0.0.0' });
   } catch (err) {
     console.error(err);
     process.exit(1);
